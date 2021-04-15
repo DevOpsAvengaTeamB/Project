734024dc33abedcd537ddc929f48c13c91de38d2
iamgavrysh <wokster110596@gmail.com>
2021-04-16 00:59:52 +0300

    remove unused vars

--- FRONTEND/variables.tf
+++ FRONTEND/variables.tf
@@ -2,12 +2,6 @@ variable "key_name" {
   default = "aws"
 }
 
-variable "aws_region" {
-  description = "AWS region to launch servers."
-  default     = "eu-central-1"
-}
-
-
 variable "aws_ami" {
   default = "ami-0767046d1677be5a0"
   }
