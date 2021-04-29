6f7fcf8d6255665fe43d908d1d43fa1af398fd7c
IrynaBb <58744461+IrynaCat@users.noreply.github.com>
2021-04-29 23:37:02 +0300

    Update main.tf

--- terraform/modules/monitoringlogging/main.tf
+++ terraform/modules/monitoringlogging/main.tf
@@ -1,82 +1,93 @@
-module "monitoring_loging"{
-    source = "./modules/monitoringloging"
-    #version = "0.1"
+resource "aws_security_group" "monitoring" {
+  name="Monitoring"
+  description="Monitoring and loging"
+  vpc_id=var.vpc_id
 
-    ssh_key_pair = var.ssh_key_pair
-    vpc_id=var.vpc_id
-    subnet=var.subnet
-    security_group=var.security_group
+  ingress {
+    description="SHH"
+    from_port=22
+    to_port=22
+    protocol="tcp"
+    cidr_blocks=["93.170.116.0/24"]
+  }
 
-    cluster_name="monitoring-loging"
-    instance_count=2
-    ami="ami-0767046d1677be5a0"
+  ingress {
+    description="Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description="Beats(Filebeat)"
+    from_port   = 5044
+    to_port     = 5044
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description="Kibana"
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description="Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description="Node exporter (prometheus)"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description="Elasticsearch JSON"
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description="AlertManager"
+    from_port   = 9093
+    to_port     = 9093
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description="Allow all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags={
+    Name="monitoring_scgroup"
+  }
+}
+
+resource "aws_instance" "monitoring_loging"{
+    ami=var.ami
     instance_type="t2.medium"
-    min_size=2
-    max_size=4
-    security_group_rules= [
-    {
-      type        = "egress"
-      from_port   = 0
-      to_port     = 65535
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      type        = "ingress"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["93.170.116.127"]
-    },
-    {
-      type        = "ingress"
-      from_port   = 3000
-      to_port     = 3000
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      type        = "ingress"
-      from_port   = 5044
-      to_port     = 5044
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      type        = "ingress"
-      from_port   = 5601
-      to_port     = 5601
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      type        = "ingress"
-      from_port   = 9090
-      to_port     = 9090
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      type        = "ingress"
-      from_port   = 9100
-      to_port     = 9100
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      type        = "ingress"
-      from_port   = 9200
-      to_port     = 9200
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      type        = "ingress"
-      from_port   = 9093
-      to_port     = 9093
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-  ]
-
+    #key_name=""
+    #subnet=var.subnet
+    security_groups=[aws_security_group.monitoring.id]
+  
+    tags={
+       Name="Prometheus ELB"
+    }
 }
