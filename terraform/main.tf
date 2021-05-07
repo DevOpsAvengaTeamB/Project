fde7d3a73fe80d32b7423a34145d195cad07c192
Vloha <49884365+Vloha@users.noreply.github.com>
2021-05-07 12:29:02 +0300

    Some changes in backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,15 +59,18 @@ module "frontend" {
   alb-arn = module.jenkins.alb-arn
   aws_alb_listener-arn =  module.jenkins.aws_alb_listener-arn
 }
-#module "backend" {
-#  source = "./modules/Backend/"
-#  vpc-id = module.network.vpc-id
-#  subnet-priv-a-id = module.network.subnet-priv-a-id
-#  subnet-priv-b-id = module.network.subnet-priv-b-id
-#  web_private_ip = module.frontend.web_private_ip    
-#  }
-
-    
+module "backend" {
+  source = "./modules/Backend/"
+  vpc-id = module.network.vpc-id
+  subnet-priv-a-id = module.network.subnet-priv-a-id
+  subnet-priv-b-id = module.network.subnet-priv-b-id
+  subnet-pub-a-id = module.network.subnet-pub-a-id
+  subnet-pub-b-id = module.network.subnet-pub-b-id
+  alb-id = module.jenkins.alb-id
+  alb-arn = module.jenkins.alb-arn
+  aws_alb_listener-arn =  module.jenkins.aws_alb_listener-arn  
+  }
+   
 /*module "monitoring_loging"{
     source = "./modules/monitoringloging"
     #version = "0.1"
