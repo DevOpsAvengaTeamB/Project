942aa91d2168a092c8e6b601b4456aa9ec41746f
IrynaBb <58744461+IrynaCat@users.noreply.github.com>
2021-05-12 21:49:02 +0300

    Delete terraform/modules/monitoringlogging directory

--- terraform/modules/monitoringlogging/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-    description= "ID vpc"
-    value=module.network.vpc-id
-}
-
-output "subnet"{
-    description="ID private subnet in a"
-    value=module.network.subnet-priv-a-id
-}
