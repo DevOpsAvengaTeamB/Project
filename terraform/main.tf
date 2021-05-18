c24b850c0748bebe423e4efc55e26562f2c42c9e
iaMgavrysh <57216162+iaMgavrysh@users.noreply.github.com>
2021-05-18 03:02:12 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 # SET PROVIDER - AMAZON WEB SERVICES
 provider "aws" {
   region     = var.aws-region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  #access_key = var.access_key
+  #secret_key = var.secret_key
 }
 
 terraform {
