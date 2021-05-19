0823220888547b1902eef59709bdfa3f884d8812
iamgavrysh <wokster110596@gmail.com>
2021-05-20 00:48:27 +0300

    lololo

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -71,7 +71,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.eip-nat.id
   subnet_id     = aws_subnet.subnet-public-a.id
-  depends_on    = ["aws_internet_gateway.igw"]
+  depends_on    = [ aws_internet_gateway.igw ]
   tags = {
     Name = "${var.nat-name}"
   }
