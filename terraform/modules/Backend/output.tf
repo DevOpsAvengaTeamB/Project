5aa7319e9158b9b6c89bf5ca89d35a70732dcf6f
iaMgavrysh <57216162+iaMgavrysh@users.noreply.github.com>
2021-05-19 14:11:44 +0300

    Update output.tf

--- terraform/modules/Backend/output.tf
+++ terraform/modules/Backend/output.tf
@@ -1,8 +1,12 @@
-output "DBpass" {
+output "db_pass" {
   value = aws_db_instance.postgresql.password
 }
 
-output "DB_adress" {
+output "db_url" {
+  value = aws_db_instance.postgresql.address
+  description = "Private ip of db"
+}
+output "rds" {
   value = aws_db_instance.postgresql.address
   description = "Private ip of db"
 }
